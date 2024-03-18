6f066125a40fbf7910e02ba1565000f9571839b1
Bella Leber-Smeaton <bella.lebersmeaton@myob.com>
2024-03-18 23:01:30 +1100

    updated readhost messages

--- automation.ps1
+++ automation.ps1
@@ -1,9 +1,9 @@
 # create a simple script that does the git process
 
-$branchName = "release-2024" 
-$decommMessage = "TestDescription"
-$jiraCardName = "TEST-123"
-$newBranchName = "$jiraCardName-$decommMessage"
+# $branchName = "release-2024" 
+# $decommMessage = "TestDescription"
+# $jiraCardName = "TEST-123"
+# $newBranchName = "$jiraCardName-$decommMessage"
 $commitMessage = "testing commit message"
 $filesToAdd = @("/Api/Service/Huxley.API.Full.sln", "/Api/Service/packages/repositories.config", "/Api/Service/build/Nuget.ps1", "/Api/Service/Hosts/Huxley.API.Version1.WebHost/Huxley.API.Version1.WebHost.csproj")
 
@@ -22,10 +22,11 @@ function RunScript() {
     # Set-Location C:\Users\Bella.LeberSmeaton\Documents\Mira\deccomscript\testRunningScripts\testRepo
     $scriptRoot = $PSScriptRoot # holds the dir where the script is located
     Set-Location $scriptRoot
-    RunCommand("git checkout main")
-    
+
+    $branchName = Read-Host "Please provide the release branch name..."
     RunCommand("git checkout $branchName")
 
+    $newBranchName = Read-Host "Please provide the new decommision branch name..."
     RunCommand("git checkout -b $newBranchName")
 
     $versionToRemove = Read-Host "Please provide the version you wish to remove, in this formet 'YYYY.MM' eg. 2024.02 "
@@ -41,9 +42,17 @@ function RunScript() {
     }
 
     Read-Host "Press Enter to continue after making your changes"
-    
-    RunCommand("git add " + ($filesToAdd -join " "))
 
+    $fullFilePaths = @()
+    foreach ($file in $filesToAdd) {
+        $fullPath = Join-Path -Path $arlRootDirectory -ChildPath $file
+        $fullFilePaths += $fullPath
+    }
+    $filesToAddCommand = $fullFilePaths -join " "
+    RunCommand("git add $filesToAddCommand")
+    # RunCommand("git add " + ($filesToAdd -join " "))
+
+    $commitMessage = Read-Host "Please provide your commit message..."
     RunCommand("git commit -m ""$commitMessage""")
 
     RunCommand("git push --set-upstream origin $newBranchName")
